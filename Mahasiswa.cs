e932f84399fc49c7c6b13553af3f9447032b850e
Muhammad Risjad Shidqi Febian <71804153+rasjidzz@users.noreply.github.com>
2023-04-17 05:18:41 +0700

    Test Class Mahasiswa

---
+++ Mahasiswa.cs
@@ -0,0 +1,25 @@
+﻿namespace tpmodul9_1302213045
+{
+    public class Mahasiswa
+    {
+        private String nama { get; set; }
+        private String nim { get; set; }
+
+        public Mahasiswa()
+        {
+            this.nama = null;
+            this.nim = null;
+        }
+
+        public String Nama
+        {
+            get { return this.nama; }
+            set { this.nama = value; }
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine(this.nama + " " + this.nim);
+        }
+    }
+}
